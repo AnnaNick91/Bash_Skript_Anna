c872b411b0d2020b4f1f346e24c5c14636cc7846
Anna Nick <anna.nick@h-brs.de>
2023-02-06 14:28:17 +0100

    Initial commit

---
+++ mein_bash_skript.sh
@@ -0,0 +1,12 @@
+#Mit diesem Skript begrüße ich euch alle
+# Kommentare füge ich hinter ein hash Zeichen ein
+
+
+echo "Hallo zusammen" 
+
+for FILE in *.txt
+do 
+    echo $FILE
+    head -n 2 $FILE
+    tail -n 2 $FILE
+done
